1c9fdc31f0f358a4f82c8a2977c4a4fdd6090ec2
gentlemanSoup <anders.horne@gmail.com>
2023-07-04 19:31:05 +0200

    Update IPFSConstructor.cs

--- Runtime/IPFSConstructor.cs
+++ Runtime/IPFSConstructor.cs
@@ -11,7 +11,7 @@ namespace Pixygon.IPFS {
         public async Task<GameObject> ConstructIpfsObject(string template) {
             var ipfs = await IpfsBridge.GetIpfsFile<Object>(template);
             GameObject g = null;
-            if(transform == null)
+            if(gameObject == null)
                 return null;
             switch(ipfs) {
                 case VideoData data:
