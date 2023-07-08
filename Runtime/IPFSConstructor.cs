f0e0aea321ca76789c1d87ba1646a18ab602f06a
gentlemanSoup <anders.horne@gmail.com>
2023-07-08 11:53:01 +0200

    Update IPFSConstructor.cs

--- Runtime/IPFSConstructor.cs
+++ Runtime/IPFSConstructor.cs
@@ -13,8 +13,10 @@ namespace Pixygon.IPFS {
             GameObject g = null;
             if(this == null)
                 return null;
-            if(ipfs == null)
-                return null;
+            if(ipfs == null) { 
+                Debug.Log("IPFS-file is null!);
+                return null; 
+            }
             switch(ipfs) {
                 case VideoData data:
                 g = Instantiate(_videoPrefab, transform);
