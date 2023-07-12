e9c5ddd4ecfa2221f1ae1b3f3b990c707d6efb43
gentlemanSoup <anders.horne@gmail.com>
2023-07-13 01:50:17 +0200

    Update IPFSBridge.cs

--- Runtime/IPFSBridge.cs
+++ Runtime/IPFSBridge.cs
@@ -5,9 +5,7 @@ using ThreeDISevenZeroR.UnityGifDecoder;
 using UnityEngine;
 using UnityEngine.Networking;
 using Pixygon.DebugTool;
-using UnityEngine.UI;
 using WebP;
-using WebP.Experiment.Animation;
 using Object = UnityEngine.Object;
 
 namespace Pixygon.IPFS {
@@ -39,7 +37,7 @@ namespace Pixygon.IPFS {
                 case "image/jpeg":
                 return GetSprite(www.downloadHandler.data) as T;
                 case "image/webp":
-                return await LoadWebP(www.downloadHandler.data) as T;
+                return await LoadWebP(www.downloadHandler.data, hash) as T;
                 case "video/mp4":
                 case "video/quicktime":
                 return new VideoData(www.url) as T;
@@ -77,22 +75,23 @@ namespace Pixygon.IPFS {
             }
             return new Gif(gifs);
         }
-        private static async Task<Sprite> LoadWebP(byte[] bytes) {
+        private static async Task<Object> LoadWebP(byte[] bytes, string hash) {
             try {
                 var t = Texture2DExt.CreateTexture2DFromWebP(bytes, lMipmaps: true, lLinear: false,
                     lError: out var lError);
                 if (lError == Error.Success) {
-                    Debug.Log("Generated with WebP!");
                     return Sprite.Create(t, new Rect(0f, 0f, t.width, t.height), new Vector2(.5f, .5f));
                 }
                 Debug.LogError("Webp Load Error : " + lError);
             }
             catch (Exception e) {
                 Debug.Log(e);
-                return await LoadWebPAnim(bytes);
+                return await GetIpfsFile<Object>(hash);
+                //return await LoadWebPAnim(bytes);
             }
             return null;
         }
+        /*
         private static async Task<Sprite> LoadWebPAnim(byte[] bytes) {
             var r = await WebP.Experiment.Animation.WebP.LoadTexturesAsync(bytes);
             if (r == null) {
@@ -108,6 +107,7 @@ namespace Pixygon.IPFS {
             };
             return s;
         }
+        */
     }
 
     public class VideoData : Object {
